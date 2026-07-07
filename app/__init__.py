7f0146e874209a8a56aa07e5073a8e76a5034c7e
Shlok Tiwari <shlokt16@gmail.com>
2026-07-07 22:12:24 +0530

    config done

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,22 @@
 from flask import Flask
 import os
+from .config import Config
+from .extensions import db
 
-
-
-def create_app():
+def create_app(config_class: type = Config):
     app = Flask(__name__,instance_relative_config=True,template_folder="../templates",static_folder="../static")
+    
+    app.config.from_object(config_class)
+    
+    os.makedirs(app.instance_path, exist_ok =  True)
+    
+    db.init_app(app)
+    @app.get("/health")
+    def health():
+        return "OK",200
+    
+    @app.route("/")
+    def home():
+        return "It works",200
+    
+    return app 
---
+++ app/config.py
@@ -0,0 +1,14 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+
+    SECRET_KEY = os.environ.get("SECRET_KEY","the-deve-secret-change-this-app")
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL"
+        f"sqlite:///{os.path.join(BASE_DIR,'instance','contacts.db')}"
+
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
